60a773f694d854310548291cbeaa845943964698
itermaat <termaat.isabelle@gmail.com>
2018-06-02 13:23:16 -0700

    new layout and theme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,28 @@
+
+
 var phrases = ["Badger", "Jaguar", "Eagle", "Parrot", "Gecko", "Scorpion", "Shark"];
+var lettersGuessed = [];
+var wins=0;
+var guesses;
+var current;
+
 
 function getRandomInt(max){
 	max++;
 	return Math.floor(Math.random() * max);
 }
+
+function displayLength(word){
+	for(var i=0; i<word.length; i++){
+		document.write('_');
+	}
+}
+
+
+
+
+
+
 
 
 
