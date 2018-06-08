ce7bc47c42a4f78acc3c340e301a688a2e214b01
itermaat <termaat.isabelle@gmail.com>
2018-06-07 18:24:10 -0700

    add javascript for some variables into html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,53 @@
 
 
-var phrases = ["Badger", "Jaguar", "Eagle", "Parrot", "Gecko", "Scorpion", "Shark"];
-var lettersGuessed = [];
+var phrases = ["Badger", "Jaguar", "Eagle", "Parrot", "Gecko", "Scorpion", "Shark", "Dolfin", "Elephant", "Squirrel", "Penguin"];
+var lettersGuessed;
 var wins=0;
 var guesses;
-var current;
+var count = 0;
+var current = phrases[count];
+var dashedWord = dashes(current).join(" ");
+var finished = false;
 
-
-function getRandomInt(max){
-	max++;
-	return Math.floor(Math.random() * max);
-}
 
 function displayLength(word){
 	for(var i=0; i<word.length; i++){
-		document.write('_');
+		document.write("_ ");
 	}
 }
+
+function isLetter(c) {
+  return c.toLowerCase() != c.toUpperCase();
+}
+
+function repeatLetter(c){
+	for(var i=0; i<lettersGuessed.length; i++){
+		if(lettersGuessed[i] == c){
+			return true;
+		}
+	}
+	return false;
+}
+
+function printLetter(){
+	var guess = event.key;
+	console.log(guess);
+	if((isLetter(guess) == true) && (repeatLetter(guess) == false))
+		lettersGuessed.push(guess);
+
+}
+
+function dashes(word){
+	var temp = ["_  ", "_  "];
+	for(var i=0; i<word.length-2; i++){
+		temp.push("_  ");
+	}
+	return temp;
+}
+document.onkeyup = printLetter;
+
+
+
 
 
 
